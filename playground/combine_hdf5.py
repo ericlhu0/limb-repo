1de028265628aeb5e430f9748ae063ef1872d977
Eric Hu <ericlehaohu@gmail.com>
2025-01-05 16:25:53 -0500

    add scripts for data generation and to find valid configs

--- playground/combine_hdf5.py
+++ playground/combine_hdf5.py
@@ -1,14 +1,7 @@
 """Try combining hdf5s."""
 
 import os
-import sys
-from datetime import datetime
 from typing import List
-
-import h5py
-import numpy as np
-
-from limb_repo.structs import Action, LimbRepoState
 
 # Parameters
 INITIAL_SIZE = 100  # Initial size for datasets (adjust as needed)
@@ -17,10 +10,8 @@ GROWTH_FACTOR = 2  # Factor to grow the dataset size when needed
 temp_dir = "/Users/eric/Documents/lr-dir/limb-repo/playground/out/temp"
 final_file_path = "/Users/eric/Documents/lr-dir/limb-repo/playground/out/final.hdf5"
 
-import os
 
-
-def find_all_files_recursively(directory):
+def find_all_files_recursively(directory) -> List[str]:
     """Find all files recursively in a given directory."""
     all_files = []
     for root, _, files in os.walk(directory):
@@ -29,7 +20,4 @@ def find_all_files_recursively(directory):
     return all_files
 
 
-# Sort files based on numeric prefix
-all_files = find_all_files_recursively(temp_dir)
-
-print(all_files)
+print(find_all_files_recursively(temp_dir))
